720e53513b93d4a09a3f88d2d0ca8238dc19e53d
Priyanka Katre <priyankack1707@gmail.com>
2024-10-16 00:33:51 +0100

    add winston and morgan

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
+const morganFormat = ":method :url :status :response-time ms";
 app.use(express.json());
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // It is good practice to add post number in .env file otherwise app will crash during deployment
 const PORT = process.env.PORT || 3000;
