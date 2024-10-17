8fa629e80a809a4399204d1589bbe69f31c7ef7b
Priyanka Katre <priyankack1707@gmail.com>
2024-10-17 23:38:23 +0100

    Add multer and Cloudinary support

--- yt/src/app.js
+++ yt/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import healthCheckRouter from "./routes/healthcheck.routes.js"
+import cookieParser from 'cookie-parser'
 
 const app = express();
+
+app.use(cookieParser);
 
 app.use(
   cors({
